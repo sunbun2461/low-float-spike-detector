b0386c502ee7d5a236e15bb6a925239d4cb3715c
Thomas Rio <a.thomas.rio@gmail.com>
2023-05-25 23:13:32 -0400

    added spikes folder and spike_dectector.py

---
+++ alpha_hist_data_2/file_count.py
@@ -0,0 +1,6 @@
+import os
+
+path = "/Users/thomasario/Coding/AI/Low_float_ground_up/alpha_hist_data"
+alpha_hist_data_2 = len(os.listdir(path))
+
+print("Number of files in folder:", alpha_hist_data_2)
